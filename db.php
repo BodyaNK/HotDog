5ff82ecfc21b8566220c49b3f4c56ba30b8ab593
Bogdan <39878171+BodyaNK@users.noreply.github.com>
2019-07-31 00:28:47 +0300

    Modified

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    $connection = mysqli_connect('ftp.bnikitchuk.icu', 'bogdan', 'kzL4B8g9vUNPS5q', 'hotdogs');
-    if (!$connection) {
-        echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
-        echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
-        echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL;
-        exit;
-    }
-?>
